eec42a03c75d59f2e634faa062a0bc0d75d7ff50
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2022-10-17 11:55:25 +0700

    Create restart.sh

---
+++ proxy/restart.sh
@@ -0,0 +1,19 @@
+# systemctl status tor
+systemctl stop tor
+systemctl start tor
+# systemctl status tor
+
+# systemctl status yggdrasil
+systemctl stop yggdrasil
+systemctl start yggdrasil
+# systemctl status yggdrasil
+
+# systemctl status i2pd.service
+systemctl stop i2pd.service
+systemctl start i2pd.service
+# systemctl status i2pd.service
+
+# systemctl status tinyproxy.service
+systemctl stop tinyproxy.service
+systemctl start tinyproxy.service
+# systemctl status tinyproxy.service
