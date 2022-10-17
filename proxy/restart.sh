2c65f3247a65c05e2091bddfcf8d56156ccd842d
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2022-10-17 11:57:50 +0700

    Update restart.sh

--- proxy/restart.sh
+++ proxy/restart.sh
@@ -1,19 +1,16 @@
-# systemctl status tor
 systemctl stop tor
 systemctl start tor
-# systemctl status tor
 
-# systemctl status yggdrasil
 systemctl stop yggdrasil
 systemctl start yggdrasil
-# systemctl status yggdrasil
 
-# systemctl status i2pd.service
 systemctl stop i2pd.service
 systemctl start i2pd.service
-# systemctl status i2pd.service
 
-# systemctl status tinyproxy.service
 systemctl stop tinyproxy.service
 systemctl start tinyproxy.service
-# systemctl status tinyproxy.service
+
+systemctl is-active --quiet tor && echo "Tor is running"
+systemctl is-active --quiet i2pd.service && echo "I2P is running"
+systemctl is-active --quiet yggdrasil && echo "Yggdrasil is running"
+systemctl is-active --quiet tinyproxy.service && echo "Tinyproxy is running"
