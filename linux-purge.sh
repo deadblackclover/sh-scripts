12ace80029453117bbb70bb1699d819d9d71836f
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2023-08-12 12:36:36 +0700

    Update linux-purge.sh

--- linux-purge.sh
+++ linux-purge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt purge -y linux-headers-5.4.0-$LINUX_VER \
-    linux-headers-5.4.0-$LINUX_VER-generic \
-    linux-image-5.4.0-$LINUX_VER-generic \
-    linux-modules-5.4.0-$LINUX_VER-generic \
-    linux-modules-extra-5.4.0-$LINUX_VER-generic
+sudo apt purge -y linux-headers-$LINUX_VER \
+    linux-headers-$LINUX_VER-generic \
+    linux-image-$LINUX_VER-generic \
+    linux-modules-$LINUX_VER-generic \
+    linux-modules-extra-$LINUX_VER-generic
